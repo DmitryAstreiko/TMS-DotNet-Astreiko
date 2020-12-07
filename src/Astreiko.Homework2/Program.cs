9e3c5e2c67a2469e1a0b91b003d9e16c1d7865ca
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-08 01:30:50 +0300

    feat: show day of the date

--- src/Astreiko.Homework2/Program.cs
+++ src/Astreiko.Homework2/Program.cs
@@ -6,7 +6,31 @@ namespace Astreiko.Homework2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Enter date (format date - yyyy-mm-dd): ");
+            string inPutdate = Console.ReadLine().ToString().Trim();
+
+            if (inPutdate.Length != 10)
+            {
+                Console.WriteLine("Uncorrect date!");
+                return;
+            }
+
+            if ((inPutdate.IndexOf("-") != 4) || (inPutdate.Substring(5).IndexOf("-") != 2))
+            {
+                Console.WriteLine("Uncorrect date!");
+                return;
+            }
+
+            try
+            {
+                var dateTimeNew = Convert.ToDateTime(inPutdate);
+                var valueDay = dateTimeNew.DayOfWeek;
+                Console.WriteLine($"Day is - {valueDay}");
+            }
+            catch
+            {
+                Console.WriteLine("Uncorrect date!");
+            }
         }
     }
 }
