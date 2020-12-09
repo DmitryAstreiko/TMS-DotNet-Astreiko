c330bed86dce829e1f7bcfd6fc7ca70203621479
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-09 20:04:34 +0300

    feat: make second version to work with date

--- src/Astreiko.Homework2/Program.cs
+++ src/Astreiko.Homework2/Program.cs
@@ -6,7 +6,18 @@ namespace Astreiko.Homework2
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter date (format date - yyyy-mm-dd): ");
+            //version 1
+            WorkWithDate1();
+            Console.WriteLine("");
+
+            //version 2            
+            WorkWithDate2();
+            Console.ReadLine();
+        }
+
+        public static void WorkWithDate1()
+        {
+            Console.Write("Version 1. Enter date (format date - yyyy-mm-dd): ");
             string inPutdate = Console.ReadLine().ToString().Trim();
 
             if (inPutdate.Length != 10)
@@ -30,6 +41,23 @@ namespace Astreiko.Homework2
             catch
             {
                 Console.WriteLine("Uncorrect date!");
+                return;
+            }           
+        }
+
+        public static void WorkWithDate2()
+        {
+            Console.Write("Version 2. Enter date: ");
+            try
+            {
+                var inPutDate = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Day is - {inPutDate.DayOfWeek}");
+            }
+            catch
+            {
+                Console.WriteLine("Uncorrect date!");
+                return;
             }
         }
     }
