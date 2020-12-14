e11c34ac74b488ce75296cfb4d41f2f52008fee2
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-14 03:31:03 +0300

    feat: part2 (bad work with equals string and enum)

--- src/Astreiko.Homework3/Program.cs
+++ src/Astreiko.Homework3/Program.cs
@@ -8,27 +8,66 @@ namespace Astreiko.Homework3
 
         private static DateTime inputFinishDate { get; set; }
 
+        private enum Days
+        {
+            Monday = 1,
+            Tuesday = 2,
+            Wednesday = 3,
+            Thursday = 4,
+            Friday = 5,
+            Saturday = 6,
+            Sunday = 7
+        }
+
         static void Main(string[] args)
         {
-            if(!InputDates()) return;
+            //if(!InputDates()) return;
 
-            if (!CheckStartFinish()) 
-            {
-                Console.WriteLine("Error! START must become earlier then FINISH date.");
-                return;
+            //if (!CheckStartFinish()) return;
+
+            //Console.WriteLine("---------------------------------------");
+            //Console.WriteLine($"Start date - {inputFirstDate}");
+            //Console.WriteLine($"Start date - {inputFinishDate}");
+            //Console.WriteLine("---------------------------------------");
+
+            var www = CheckInputDay();
+
+            Console.WriteLine($"{www}");
+        } 
+
+        private static string CheckInputDay()
+        {
+            string vRes = "123";
+            Console.Write("Enter DAY to search - ");
+
+            var inputDay = Console.ReadLine();
+
+            if (inputDay.Equals(Enum.Parse(Days.Monday))) 
+            { 
+                vRes = Days.Monday.ToString(); 
             }
 
-            Console.WriteLine("---------------------------------------");
-            Console.WriteLine($"Start date - {inputFirstDate}");
-            Console.WriteLine($"Start date - {inputFinishDate}");
-            Console.WriteLine("---------------------------------------");
-        } 
+            //string qqq;
+            //Enum.TryParse(inputDay, out qqq)
+
+            ////Name myName;
+            //if (Enum.TryParse<Name>(nameString, out myName))
+            //{
+            //    switch (myName) { case John: ... }
+            //}
+
+            return vRes;
+        }
 
         private static bool CheckStartFinish()
         {
             var vRes = true;
 
-            if (inputFirstDate > inputFinishDate) vRes = false;
+            if (inputFirstDate > inputFinishDate)
+            {
+                vRes = false;
+                Console.WriteLine("Error! START must become earlier then FINISH date.");
+            }
 
             return vRes;
         }
@@ -38,11 +77,11 @@ namespace Astreiko.Homework3
             var vRes = true;
             try
             {
-                Console.Write("Enter start date:");
+                Console.Write("Enter start date : ");
 
                 inputFirstDate = DateTime.Parse(Console.ReadLine());
 
-                Console.Write("Enter finish date:");
+                Console.Write("Enter finish date : ");
 
                 inputFinishDate = DateTime.Parse(Console.ReadLine());
             }
