62ab7541664395d22871caca92e8bfc375281509
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-14 22:13:58 +0300

    feat: add many info

--- src/Astreiko.Homework3/Program.cs
+++ src/Astreiko.Homework3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Astreiko.Homework3
 {
@@ -30,33 +31,101 @@ namespace Astreiko.Homework3
             //Console.WriteLine($"Start date - {inputFinishDate}");
             //Console.WriteLine("---------------------------------------");
 
-            var www = CheckInputDay();
+            //var www = CheckInputDay();
 
-            Console.WriteLine($"{www}");
-        } 
+            //Console.WriteLine($"{www}");
 
-        private static string CheckInputDay()
-        {
-            string vRes = "123";
-            Console.Write("Enter DAY to search - ");
+            DateTime start;
 
-            var inputDay = Console.ReadLine();
+            DateTime end;
 
-            if (inputDay.Equals(Enum.Parse(Days.Monday))) 
-            { 
-                vRes = Days.Monday.ToString(); 
+            while (true)
+            {
+                DateTime.TryParse(Console.ReadLine(), out start);
+
+                end = DateTime.Parse(Console.ReadLine());
+
+                if (start > end)
+                {
+                    Console.WriteLine("Ошибка.Попробуйте снова.");
+                    break;
+                }
             }
 
-            //string qqq;
-            //Enum.TryParse(inputDay, out qqq)
+            var dayOfWeek = Console.ReadLine();
 
-            ////Name myName;
-            //if (Enum.TryParse<Name>(nameString, out myName))
-            //{
-            //    switch (myName) { case John: ... }
-            //}
+            var list = GetDaysByUserInput(start, end, dayOfWeek);
 
-            return vRes;
+            Console.ReadKey();
+        } 
+
+        //private static string CheckInputDay()
+        //{
+        //    string vRes = "123";
+        //    Console.Write("Enter DAY to search - ");
+
+        //    var inputDay = Console.ReadLine();
+
+        //    if (inputDay.Equals(Enum.Parse(Days.Monday))) 
+        //    { 
+        //        vRes = Days.Monday.ToString(); 
+        //    }
+
+        //    //string qqq;
+        //    //Enum.TryParse(inputDay, out qqq)
+
+        //    ////Name myName;
+        //    //if (Enum.TryParse<Name>(nameString, out myName))
+        //    //{
+        //    //    switch (myName) { case John: ... }
+        //    //}
+
+        //    return vRes;
+        //}
+
+        static List<DateTime> GetDaysByUserInput(DateTime start, DateTime end, string dayOfWeek)
+        {
+            var filterDays = new List<DateTime>();
+
+            //while(true)
+            // {
+            //     start = start.AddDays(1);
+            // }
+
+            // for (int i = 0; ; i++)
+            // {
+            //     if (end >= start)
+            //     {
+            //         if (start.DayOfWeek.ToString() == dayOfWeek)
+            //         {
+            //             filterDays.Add(start);
+
+            //         }
+            //         start = start.AddDays(1);
+            //         continue;
+            //     }
+
+            //     break;
+            // }
+
+            while (end >= start)
+            {
+                if (start.DayOfWeek.ToString() == dayOfWeek)
+                {
+                    filterDays.Add(start);
+                }
+                start = start.AddDays(1);
+            }
+
+            foreach (var row in filterDays)
+            {
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("----------");
+            Console.WriteLine($"{filterDays.Count}");
+
+            return filterDays;
         }
 
         private static bool CheckStartFinish()
