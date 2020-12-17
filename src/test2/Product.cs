c17ad2bf9651bc8ad887525b26a4c654908a1828
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-17 20:07:21 +0300

    init: project and class Product

---
+++ src/test2/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace test2
+{
+    abstract class Product
+    {
+        public int MyField;
+
+        public int MyProperty { get; set; }
+
+    }
+}
