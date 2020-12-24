3733f4bf747c8b67ee82ac536e18f13ad7f40b9f
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-24 19:31:57 +0300

    init: add project test6_delegate_event

---
+++ src/test6-delegate_event/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test6_delegate_event
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
