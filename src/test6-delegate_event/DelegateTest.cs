a9a69a37fdd4e7518c20f02aa847d58eb15bf026
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-24 21:53:42 +0300

    feat: лямбда-выражения

--- src/test6-delegate_event/DelegateTest.cs
+++ src/test6-delegate_event/DelegateTest.cs
@@ -36,8 +36,72 @@ namespace test6_delegate_event
         //    Console.WriteLine("Another");
         //}
 
+        ////----пример делегата ----
+        //public Action mess;
+
+        //private object _anything;
+
+        //public DelegateTest(string anything)
+        //{
+        //    _anything = anything;
+
+        //    Console.WriteLine($"{anything}");
+        //}
+
+        //public DelegateTest(int anything)
+        //{
+        //    _anything = anything;
+
+        //    Console.WriteLine($"{anything}");
+        //}
+
+        //public void ShowMessage()
+        //{
+        //    Console.WriteLine(nameof(_anything));
+        //}
+
+        //public void ShowAnotherMessage()
+        //{
+        //    Console.WriteLine("Another");
+        //}
+
+        //------------------
+
         //----пример делегата ----
-        public Action mess;
+        ////public Predicate<int> mess;
+
+        ////private object _anything;
+
+        ////public DelegateTest(string anything)
+        ////{
+        ////    _anything = anything;
+
+        ////    Console.WriteLine($"{anything}");
+        ////}
+
+        ////public DelegateTest(int anything)
+        ////{
+        ////    _anything = anything;
+
+        ////    Console.WriteLine($"{anything}");
+        ////}
+
+        ////public bool ShowMessage(int num)
+        ////{
+        ////    Console.WriteLine(nameof(_anything));
+
+        ////    return true;
+        ////}
+
+        ////public bool ShowAnotherMessage(int num)
+        ////{
+        ////    Console.WriteLine("Another");
+
+        ////    return false;
+        ////}
+        
+      //--------Func-----
+        public Func<int, string, bool> mess;
 
         private object _anything;
 
@@ -55,14 +119,18 @@ namespace test6_delegate_event
             Console.WriteLine($"{anything}");
         }
 
-        public void ShowMessage()
+        public bool ShowMessage(int num, string str)
         {
             Console.WriteLine(nameof(_anything));
+
+            return true;
         }
 
-        public void ShowAnotherMessage()
+        public bool ShowAnotherMessage(int num, string str)
         {
             Console.WriteLine("Another");
+
+            return false;
         }
     }
 }
--- src/test6-delegate_event/Program.cs
+++ src/test6-delegate_event/Program.cs
@@ -9,9 +9,37 @@ namespace test6_delegate_event
             var delegateTest1 = new DelegateTest("mess1");
             //var delegateTest2 = new DelegateTest(1);
 
-            delegateTest1.mess = delegateTest1.ShowMessage;
+            ////delegateTest1.mess = delegateTest1.ShowMessage;
+            ////delegateTest1.mess += delegateTest1.ShowAnotherMessage;
 
-            delegateTest1.mess();
+            ////delegateTest1.mess(123);
+
+            //---------лямбда выражения-----------------
+            var delegateTest2 = new DelegateTest("mess1");
+            //var delegateTest2 = new DelegateTest(1);
+
+            delegateTest2.mess = (x, y) =>
+            {
+                Console.WriteLine("qwe");
+                return true;
+            };
+
+            delegateTest2.mess += delegateTest1.ShowAnotherMessage;
+
+            delegateTest2.mess(123, "aSD");
+
+            //лямбда выражения
+            Action<int, string> action = (num, str) => Console.WriteLine($"{num} {str}");
+
+            action(111, "123");
+
+            Predicate<int> predicate = (num) => true;
+
+            var vpr = predicate(2021);
+
+            Func<int, string> func = (num) => num.ToString();
+
+            var vf = func(2021);
 
             Console.ReadKey();
         }
