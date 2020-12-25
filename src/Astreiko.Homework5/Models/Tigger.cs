60a6a2c68f5f659a5519b699f98e48b25c5be855
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:14:04 +0300

    init: add Class Tiger

---
+++ src/Astreiko.Homework5/Models/Tigger.cs
@@ -0,0 +1,25 @@
+﻿using Astreiko.Homework5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Models
+{
+    public class Tigger : AnimalsBase, IVoice, ITraffic, IFood
+    {
+        public void Say()
+        {
+            Console.WriteLine("RRRRRR");
+        }
+
+        public void Traffic()
+        {
+            Console.WriteLine("more 100 km/h");
+        }
+
+        public void WhatEat()
+        {
+            Console.WriteLine("All animals");
+        }
+    }
+}
