2ae8b3030f2e9e2b46c5f07dc62ef392852ee374
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:33:19 +0300

    init: add class Giraffe

---
+++ src/Astreiko.Homework5/Models/Giraffe.cs
@@ -0,0 +1,25 @@
+﻿using Astreiko.Homework5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Models
+{
+    public class Giraffe : AnimalsBase, IVoice, ITraffic, IFood
+    {
+        public void Say()
+        {
+            Console.WriteLine("YYYYYYYYYYYY");
+        }
+
+        public void Traffic()
+        {
+            Console.WriteLine("About 40 km/h");
+        }
+
+        public void WhatEat()
+        {
+            Console.WriteLine("Leaves ...");
+        }
+    }
+}
