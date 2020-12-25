6187987e1690f75694f703b73b6a0bac29a0e89c
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:01:49 +0300

    init: ITraffic

---
+++ src/Astreiko.Homework5/Interfaces/ITraffic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Interfaces
+{
+    /// <summary>
+    /// Скорость движения
+    /// </summary>
+    public interface ITraffic
+    {
+        void Traffic();
+    }
+}
