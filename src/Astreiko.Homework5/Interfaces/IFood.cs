d7a3462e237af02b2f8f83e118ec806a6c652965
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:06:30 +0300

    init: add IFood

---
+++ src/Astreiko.Homework5/Interfaces/IFood.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Interfaces
+{
+    /// <summary>
+    /// Что ест животное
+    /// </summary>
+    public interface IFood
+    {
+        void WhatEat();
+    }
+}
