2cb2b54dbc26f6bd6dec57c337df11ac26bc1e55
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 01:47:24 +0300

    init: add enum KindAnimals, abstract class AnumalBase

---
+++ src/Astreiko.Homework5/Helper/KindAnimals.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Helper
+{
+    enum KindAnimals
+    {
+        Mammals = 1,
+        Pets = 2,
+        AfricanAnimals =3,
+    }
+}
---
+++ src/Astreiko.Homework5/Models/AnimalsBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Models
+{
+    public abstract class AnimalsBase
+    {
+
+    }
+}
