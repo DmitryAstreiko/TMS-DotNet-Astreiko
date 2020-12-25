96d866f6daee217cbeb66bb79f77d9164b9a5fe5
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:28:48 +0300

    init: add class Bear

---
+++ src/Astreiko.Homework5/Models/Bear.cs
@@ -0,0 +1,25 @@
+﻿using Astreiko.Homework5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Models
+{
+    public class Bear : AnimalsBase, IVoice, ITraffic, IFood
+    {
+        public void Say()
+        {
+            Console.WriteLine("GEEEEEEEE");
+        }
+
+        public void Traffic()
+        {
+            Console.WriteLine("About 60 km/h");
+        }
+
+        public void WhatEat()
+        {
+            Console.WriteLine("Honey ...");
+        }
+    }
+}
