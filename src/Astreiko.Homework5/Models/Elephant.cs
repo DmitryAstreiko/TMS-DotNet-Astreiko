75ea9027cf6a868ccf4be3d6c590d5825ebb42a9
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-26 02:27:19 +0300

    init: add class elephant

---
+++ src/Astreiko.Homework5/Models/Elephant.cs
@@ -0,0 +1,25 @@
+﻿using Astreiko.Homework5.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Astreiko.Homework5.Models
+{
+    public class Elephant : AnimalsBase, IVoice, ITraffic, IFood
+    {
+        public void Say()
+        {
+            Console.WriteLine("YYYYYYYY");
+        }
+
+        public void Traffic()
+        {
+            Console.WriteLine("About 40 km/h");
+        }
+
+        public void WhatEat()
+        {
+            Console.WriteLine("Grass ...");
+        }
+    }
+}
