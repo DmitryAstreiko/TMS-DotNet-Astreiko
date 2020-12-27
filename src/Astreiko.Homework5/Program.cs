afe03eb3282928888ee02c4699f5058f48f5de34
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-27 23:46:00 +0300

    feat: add body Main and menu operations

--- src/Astreiko.Homework5/Program.cs
+++ src/Astreiko.Homework5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Astreiko.Homework5.Models;
 using System;
+using System.Collections.Generic;
 
 namespace Astreiko.Homework5
 {
@@ -7,15 +8,56 @@ namespace Astreiko.Homework5
     {
         static void Main(string[] args)
         {
-            var eee = new Tigger();
+            
 
-            Console.WriteLine($"{eee.WhatEat} {eee.WhatSay}");
+            ShowMenu();
 
-            eee.WhatEat = "12312321";
+            var check = true;
 
-            Console.WriteLine($"{eee.WhatEat} {eee.WhatSay}");
+            while (check)
+            {
+                Console.WriteLine();
+                Console.Write("Please press any key : ");
 
+                var inputChar = Console.ReadLine().Trim();
+
+                switch (inputChar)
+                {
+                    case "s":
+
+                        ShowMenu();
+                        break;
+                    case "a":
+
+                        ShowMenu();
+                        break;
+                    case "c":
+                        check = false;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine("Exit from application.");
+                        Console.ResetColor();
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Uncorrect key.");
+                        Console.ResetColor();
+                        ShowMenu();
+                        break;
+                } 
+            }
+            
             Console.ReadKey();
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("Menu operations : ");
+
+            Console.WriteLine("Show list animals - press s");
+
+            Console.WriteLine("Add animal (You can add Bear, Cat, Elephant, Giraffe, Tiger) - press a");
+
+            Console.WriteLine("Exit - press c");
         }
     }
 }
