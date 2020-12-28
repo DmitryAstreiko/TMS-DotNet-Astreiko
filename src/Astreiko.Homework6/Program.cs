5731e116ee69eb70367b9d3894628b84eccfe4f4
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2020-12-29 00:06:37 +0300

    init: add method show MainMenu

--- src/Astreiko.Homework6/Program.cs
+++ src/Astreiko.Homework6/Program.cs
@@ -6,7 +6,52 @@ namespace Astreiko.Homework6
     {
         static void Main(string[] args)
         {
-            
+            ShowMainMenu();
+
+            var check = true;
+
+            while (check)
+            {
+                Console.WriteLine();
+                Console.Write("Please choose the operation : ");
+
+                var inputChoose = Console.ReadLine().Trim();
+
+                switch (inputChoose)
+                {
+                    case "a":
+
+                        break;
+                    case "w":
+                        break;
+                    case "d":
+                        break;
+                    case "e":
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Application will be close.");
+                        Console.ResetColor();
+                        check = false;
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Entered uncorect char. Please try again.");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ShowMainMenu() 
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Choose operations : ");
+            Console.WriteLine("Top up an account - [a]");
+            Console.WriteLine("Withdrawal of funds - [w]");
+            Console.WriteLine("Display actual balance - [d]");
+            Console.WriteLine("Exit - [e]");
+            Console.ResetColor();
         }
     }
 }
