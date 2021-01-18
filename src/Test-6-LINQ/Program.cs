822165368f72d134fde93affd3f65e50ddca2f44
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-01-18 20:10:57 +0300

    init: add project

---
+++ src/Test-6-LINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_6_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
