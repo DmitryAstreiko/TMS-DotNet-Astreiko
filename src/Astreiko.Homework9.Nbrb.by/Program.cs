096364bc19c569cc853d3ce5384cf598fd976674
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-01-29 00:55:42 +0300

    Nbrb: add project, 3 class, select date or period

---
+++ src/Astreiko.Homework9.Nbrb.by/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Astreiko.Homework9.Nbrb.by;
+using Astreiko.Homework9.Nbrb.@by.UI;
+using Astreiko.Homework9.Nbrb.@by.UI.Enums;
+
+namespace Astreiko.Homework9.Nbrb.by
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ToDo();   
+        }
+
+        private static void ToDo()
+        {
+           var uiClass = new UIClass();
+
+           uiClass.GetMenuSelectDate();
+
+           var inputNumDate = uiClass.GetVariantDate();
+
+           switch (inputNumDate)
+           {
+               case TypeSelectDates.OneDate:
+                   break;
+               case TypeSelectDates.PeriodDate:
+                   break;
+               case TypeSelectDates.None:
+                   break;
+           }
+
+           Console.ReadKey();
+        }
+    }
+}
---
+++ src/Astreiko.Homework9.Nbrb.by/UI/UIClass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Astreiko.Homework9.Nbrb.@by.UI.Enums;
+
+namespace Astreiko.Homework9.Nbrb.by.UI
+{
+    public class UIClass
+    {
+        public void GetMenuSelectDate()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Please choose the operation : ");
+            Console.WriteLine("Input date for select currency - (1)");
+            Console.WriteLine("Input period for select currency - (2)");
+            Console.WriteLine("Exit - [3]");
+            Console.ResetColor();
+            Console.WriteLine("-----");
+        }
+
+        public TypeSelectDates GetVariantDate()
+        {
+            while (true)
+            {
+                Console.Write("Select operation: ");
+                var numInput = int.Parse(Console.ReadLine().Trim());
+
+                switch (numInput)
+                {
+                    case 1:
+                        return TypeSelectDates.OneDate;
+                    case 2:
+                        return TypeSelectDates.PeriodDate;
+                    case 3:
+                        return TypeSelectDates.None;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Entered uncorrect number. Please try again.");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+
+        }
+    }
+}
