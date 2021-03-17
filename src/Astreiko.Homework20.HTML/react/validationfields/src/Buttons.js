d88045db7c25c720aa88f168b79b76c4530b1547
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-03-17 03:44:00 +0300

    HTML. React. Simple commit

---
+++ src/Astreiko.Homework20.HTML/react/validationfields/src/Buttons.js
@@ -0,0 +1,10 @@
+function Buttons() {
+    return (
+        <div className="buttons-section">
+            <button type="submit" className="button-validate">Submit</button>
+            <button className="button-validate">Cancel</button>
+        </div>
+    )
+}
+
+export default Buttons
--- src/Astreiko.Homework20.HTML/react/validationfields/src/ValidateForm.js
+++ src/Astreiko.Homework20.HTML/react/validationfields/src/ValidateForm.js
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
+//import Buttons from './Buttons'
+
+function resetValue() {
+    const fName = document.querySelector("first-name")
+    
+}
+
+
 
 function ValidateForm() {
+    const [firstNameValue, setFirstNameValue] = useState('')
+
     return (
+    
         <div className="validate-content">
             <h3 style={{textAlign: 'center'}}>Registration information</h3>            
             <div><label>First Name:</label></div>
-            <div><input className="text-validate" type="text" placeholder='First name'/></div>
+            <div><input value={firstNameValue} className="text-validate" id="first-name" type="text" placeholder='First name'/></div>
             <div><label>Last Name:</label></div>
-            <div><input className="text-validate" type="text" placeholder='Last name'/></div>
+            <div><input className="text-validate" id="last-name" type="text" placeholder='Last name'/></div>
             <div><label>Password:</label></div>
-            <div><input className="text-validate" type="text" placeholder='Password'/></div>
+            <div><input className="text-validate" id="password" type="text" placeholder='Password'/></div>
             <div>
                 <label>Accept terms and conditions.</label>
-                <input type="checkbox"></input>
+                <input type="checkbox" id="accept"></input>
             </div>
             <div className="buttons-section">
-                <button className="button-validate">Submit</button>
-                <button className="button-validate">Cancel</button>
-            </div>
+            <button type="submit" className="button-validate">Submit</button>
+            <button className="button-validate" onReset={resetValue()}>Cancel</button>
+        </div>
         </div>
     )
 }
